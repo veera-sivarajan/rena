78de0bda4264612ae24ebb74c79f2d73374ce015
Veera <sveera.2001@gmail.com>
2021-10-10 20:35:44 -0400

    Implement RError for Runtime Errors
    Previously I wrapped all possible return types(including Error)
    into a enum but people on discord say its a bad idea. So I
    tried implementing a seperate struct for representing runtime
    errors and making all the evaluate functions return a
    Result<Value, RError>.
    
    Based on how the interpreter behaves, there's no big difference
    between this and my previous approach. However this approach
    makes is very clear for the reader to understand that an
    evaluate function will either return a value or an error at the
    cost of writing more code. Whereas my previous approach required
    less code but was implicit about its return value. It did not
    explicitly say that a function could return a error too.
    
    I understand that writing rust is all about making things
    explicit in the codebase without hiding anything. But I wonder
    if it's really useful if it comes at the cost of writing more
    code.

--- src/err.rs
+++ src/err.rs
@@ -23,4 +23,26 @@ impl LoxError {
         LoxError{ err }
     }
 }
+
+#[derive(Debug)]
+pub struct RError {
+    err: String,
+}
+
+impl RError {
+    pub fn new(err: String) -> RError {
+        RError{ err }
+    }
+}
+
+impl Error for RError {
+    fn description(&self) -> &str {
+        &self.err
+    }
+}
     
+impl fmt::Display for RError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Runtime Error: {}", &self.err)
+    }
+}
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,11 +1,11 @@
 use crate::expr::{Expr, BinaryExpr, NumberExpr, UnaryExpr};
 use crate::token::TokenType;
+use crate::err::RError;
 
 pub enum Value {
     Number(f64),
     Bool(bool),
     String(String), 
-    Error(String),
 }
 
 fn stringify(result: Value) -> String {
@@ -13,16 +13,17 @@ fn stringify(result: Value) -> String {
         Value::Number(num) => format!("{}", num),
         Value::Bool(tof) => format!("{}", tof),
         Value::String(value) => format!("{}", value),
-        Value::Error(message) => format!("Error: {}", message),
     }
 }
 
 pub fn interpret(expression: Expr) {
-    let result = evaluate(expression);
-    println!("{}", stringify(result));
+    match evaluate(expression) {
+        Ok(value) => println!("{}", stringify(value)),
+        Err(runtime_error) => println!("{}", runtime_error.to_string()),
+    }
 }
 
-fn evaluate(expression: Expr) -> Value {
+fn evaluate(expression: Expr) -> Result<Value, RError> {
     match expression {
         Expr::Binary(expr) => intpt_binary(expr),
         Expr::Unary(expr)  => intpt_unary(expr),
@@ -32,56 +33,58 @@ fn evaluate(expression: Expr) -> Value {
     }
 }
 
-fn intpt_unary(expression: UnaryExpr) -> Value {
-    let right = evaluate(*expression.right);
+fn intpt_unary(expression: UnaryExpr) -> Result<Value, RError> {
+    let right = evaluate(*expression.right)?;
     match expression.oper.token_type {
         TokenType::Minus => {
             match right {
-                Value::Number(num) => Value::Number(-num),
-                _ => Value::Error(String::from("Operand should be a number.")),
+                Value::Number(num) => Ok(Value::Number(-num)),
+                _ => Err(RError::new(
+                    String::from("Operand should be a number."))),
             }
         },
         TokenType::Bang =>{
             match right {
-                Value::Bool(value) => Value::Bool(!value),
-                _ => Value::Bool(false),
+                Value::Bool(value) => Ok(Value::Bool(!value)),
+                _ => Ok(Value::Bool(false)),
             }
         },
-        _ => Value::Error(String::from("Unknown unary operation")),
+        _ => Err(RError::new(String::from("Unknown unary operation"))),
     }
 }
 
-fn intpt_boolean(expression: bool) -> Value {
-    Value::Bool(expression)
+fn intpt_boolean(expression: bool) -> Result<Value, RError> {
+    Ok(Value::Bool(expression))
 }
 
-fn intpt_string(expression: String) -> Value {
-    Value::String(expression)
+fn intpt_string(expression: String) -> Result<Value, RError> {
+    Ok(Value::String(expression))
 }
 
-fn intpt_number(expression: NumberExpr) -> Value {
-    Value::Number(expression.value)
+fn intpt_number(expression: NumberExpr) -> Result<Value, RError> {
+    Ok(Value::Number(expression.value))
 }
 
-fn intpt_binary(expression: BinaryExpr) -> Value {
-    let left = evaluate(*expression.left);
-    let right = evaluate(*expression.right);
+fn intpt_binary(expression: BinaryExpr) -> Result<Value, RError> {
+    let left = evaluate(*expression.left)?;
+    let right = evaluate(*expression.right)?;
 
     match (left, right) {
         (Value::Number(l), Value::Number(r)) => {
             match expression.oper.token_type {
-                TokenType::Plus => Value::Number(l + r),
-                TokenType::Minus => Value::Number(l - r),
-                TokenType::Slash => Value::Number(l / r),
-                TokenType::Star => Value::Number(l * r),
-                TokenType::Greater => Value::Bool(l > r),
-                TokenType::GreaterEqual => Value::Bool(l >= r),
-                TokenType::Less => Value::Bool(l < r),
-                TokenType::LessEqual => Value::Bool(l <= r),
-                _ => Value::Error(String::from("Unknown binary operation")),
+                TokenType::Plus         => Ok(Value::Number(l + r)),
+                TokenType::Minus        => Ok(Value::Number(l - r)),
+                TokenType::Slash        => Ok(Value::Number(l / r)),
+                TokenType::Star         => Ok(Value::Number(l * r)),
+                TokenType::Greater      => Ok(Value::Bool(l > r)),
+                TokenType::GreaterEqual => Ok(Value::Bool(l >= r)),
+                TokenType::Less         => Ok(Value::Bool(l < r)),
+                TokenType::LessEqual    => Ok(Value::Bool(l <= r)),
+
+                _ => Err(RError::new(String::from("Unknown binary operation."))),
             }
         },
-        _ => Value::Error(String::from("Operands should be numbers.")),
+        _ => Err(RError::new(String::from("Operands should be numbers."))),
     }
 }
     
